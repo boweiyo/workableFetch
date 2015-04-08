db3cfbc7238c621e301e11683a124c88f3771452
boweiyo <bowei@chotot.vn>
2015-04-08 17:14:54 +0700

    Initial Commit

---
+++ WorkingWithClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WorkingWithClass
+//
+//  Created by Bowei on 4/8/15.
+//  Copyright (c) 2015 Teo Bowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
